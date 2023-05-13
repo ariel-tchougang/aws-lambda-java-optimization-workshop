c11402aec84ebf8ce6b4a210675b602a1694b9c8
Ariel Tchougang <ariel.tchougang@gmail.com>
2023-05-13 15:41:52 +0000

    Added a pause time to let logs flow to CloudWatch logs before launching query

--- benchmark.sh
+++ benchmark.sh
@@ -17,24 +17,28 @@ API_GW_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query
 if [ $APP == "plain-java" ]; then
   echo "Running Artillery on plain-java-request-handler"
   artillery run -t $API_GW_URL loadtest/plain-java-request-handler.yaml
+  sleep 30
   exit 0
 fi
 
 if [ $APP == "serverless-java-container" ]; then
   echo "Running Artillery on serverless-java-container"
   artillery run -t $API_GW_URL loadtest/serverless-java-container.yaml
+  sleep 30
   exit 0
 fi
 
 if [ $APP == "spring" ]; then
   echo "Running Artillery on springboot-function-handler"
   artillery run -t $API_GW_URL loadtest/springboot-function-handler.yaml
+  sleep 30
   exit 0
 fi
 
 if [ $APP == "micronaut" ]; then
   echo "Running Artillery on micronaut-request-handler"
   artillery run -t $API_GW_URL loadtest/micronaut-request-handler.yaml
+  sleep 30
   exit 0
 fi
 
